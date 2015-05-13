ede04dfc8c913fc5e6389213d4992427f6eea304
Walter McGinnis <wm@waltermcginnis.com>
2015-05-13 12:17:25 +1200

    Fix typo in doc string

--- src/plupload.js
+++ src/plupload.js
@@ -710,7 +710,7 @@ plupload.addFileFilter('prevent_duplicates', function(value, file, cb) {
 	@param {String} [settings.flash_swf_url] URL of the Flash swf.
 	@param {Object} [settings.headers] Custom headers to send with the upload. Hash of name/value pairs.
 	@param {Number} [settings.max_retries=0] How many times to retry the chunk or file, before triggering Error event.
-	@param {String} [settings.multipart="post"] Either post or put.
+	@param {String} [settings.method="post"] Either post or put.
 	@param {String} [settings.content_type="application/octet-stream"] For some upload hosts, you may need to set this to match file content_type exactly.
 	@param {Boolean} [settings.multipart=true] Whether to send file and additional parameters as Multipart formated message.
 	@param {Object} [settings.multipart_params] Hash of key/value pairs to send with every file upload.
